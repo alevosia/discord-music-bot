98ab590d31879fd3fd9e146148432ef6438c3011
alevfalse <38272871+alevfalse@users.noreply.github.com>
2018-06-03 23:17:50 +0800

    changed the order of variables

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
+# make sure to update these for your own server
 TOKEN = 'NDQ0NTA0NjI5MDAzNDE5NjU4.DeauIg.xztERXdhIVoz5uL1f1nsHphcDpI'
 OwnerID = 374447686319079425
-YoutubeAPIKey = 'AIzaSyBhYruEtZeGsYQJly4sdUgwTr0HSmcPpqQ'
-prefix = '!'
-description = 'A music bot for Unforsaken Haven\'s Discord server.'
-
 ServerID = 451668403342802954
 MusicSpamChannelID = 451719192391974912
 MusicVoiceChannelID = 451668482694709259
+
+prefix = '!'
+description = 'A music bot for Unforsaken Haven\'s Discord server.'
 
 SearchTimeOut = 30
 MaxSearchResults = 5
@@ -15,5 +15,6 @@ MaxSongDuration = 10800
 MaxUserQueue = 20
 MaxMusicQueue = 30
 
+YoutubeAPIKey = 'AIzaSyBhYruEtZeGsYQJly4sdUgwTr0HSmcPpqQ'
 QueueImageURL = 'https://78.media.tumblr.com/6bb79faf55461ee01a87d5bedb9081f1/tumblr_onn734q6Rv1ue08b9o1_500.gif'
 NowPlayingImagURL = 'http://vinylgif.com/gifs/201412/spinning-7-inch.gif'
